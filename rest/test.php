ba5dbca05459e7f652bb34b693a766172b471fb6
Steffen Pegenau <steffen.pegenau@gmail.com>
2015-05-27 16:34:40 +0200

    Implementation test of a word cloud with d3

--- rest/test.php
+++ rest/test.php
@@ -26,9 +26,10 @@ class activityCloud {
     private $data;
     private $modinfo;
     private $sumOfEvents;
-    private $FONT_SIZE_MAX = 50;
-    private $FONT_SIZE_MIN = 10;
+    private $FONT_SIZE_MAX = 100;
+    private $FONT_SIZE_MIN = 40;
     private $STEPS = 5;
+    private $STEP_WIDTH = 0;
 
     public function __construct($courseid) {
         $this->courseid = $courseid;
@@ -42,6 +43,8 @@ class activityCloud {
         $this->data = $this->getData();
 
         $this->sumOfEvents = $this->calculateSumOfEvents();
+        
+        $this->STEP_WIDTH = ($this->FONT_SIZE_MAX - $this->FONT_SIZE_MIN) / 5;
     }
 
     private function calculateSumOfEvents() {
@@ -89,38 +92,123 @@ class activityCloud {
         return $data;
     }
 
-    public function printCloud() { 
-       $buffer = "";
-
-        $buffer .= '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="300" width="50%">';
-
-        //$buffer .= '<text font-family="impact" x="50" y="86">';
-        foreach ($this->data as $cmid => $a) {
-            $weight = 10 * $a['numberOfEvents'] * $a['numberOfEvents'] / $this->sumOfEvents;
-            $size = ($this->FONT_SIZE_MAX - $this->FONT_SIZE_MIN) / $this->STEPS;
-            $weighted_size = $this->FONT_SIZE_MIN + floor($weight * $size);
-            
-            
-            $buffer .= '<a xlink:href="grafik.svg">'
-                    . '<tspan font-size="'. $weighted_size . '">' . $a['name'] .'</tspan>'
-                    . '</a>';
-            
-            /*
-            $buffer .= '<a xlink:href="' . $a['moodleURL'] . '"
-                            xlink:title="' . $a['name'] . '">
-                            <rect x="10" y="60" ry="5" width="40" height="40"
-                            style="fill:mintcream; stroke:black;" />
-                        </a>';
-             * 
-             */
-            //$buffer .= '<tspan font-size="'. $weighted_size . '"><a href"' . $a['moodleURL'] . '">' . $a['name'] .'</a></tspan>';
+    public function dataAsJSON() {
+        $return = array();
+        foreach ($this->data as $id => $data) {
+            $weigth = $data['numberOfEvents'] / $this->sumOfEvents;
+            //$size = $this->FONT_SIZE_MIN + $weigth * $this->STEP_WIDTH;
+            $size = $weigth * $this->FONT_SIZE_MAX;
+            $data['size'] = $size;
+            if($size > 0) {
+                $return[] = $data;
+            }
         }
-        //$buffer .= '</text>';
-        $buffer .= '</svg>';
-        //echo "<html><body>";
+        $return = array('name' => 'root', 'children' => $return);
+        return json_encode($return);
+    }
+
+    public function printCloud() {
+        global $CFG;
+        $buffer = "";
+        $buffer .= '
+                <!DOCTYPE html>
+                <meta charset="utf-8">
+                <style>
+
+                text {
+                  font: 10px sans-serif;
+                }
+
+                </style>
+                <body>
+                <script src="' . $CFG->wwwroot .'/mod/learninganalytics/js/d3.v3.min.js"></script>
+                <script>
+                var diameter = 600,
+                    format = d3.format(",d"),
+                    color = d3.scale.category20c();
+
+                var bubble = d3.layout.pack()
+                    .sort(null)
+                    .size([diameter, diameter])
+                    .padding(1.5);
+
+                var svg = d3.select("body").append("svg")
+                    .attr("width", diameter)
+                    .attr("height", diameter)
+                    .attr("class", "bubble");
+                    
+                var json = ' . $this->dataAsJSON() . ';
+
+                //d3.json(json, 
+                var process = function(error, root) {
+                  var node = svg.selectAll(".node")
+                      .data(bubble.nodes(classes(root))
+                      .filter(function(d) { return !d.children; }))
+                    .enter().append("g")
+                      .attr("class", "node")
+                      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+                  node.append("title")
+                      .text(function(d) { return d.className + ": " + format(d.value); });
+
+                  node.append("circle")
+                      .attr("r", function(d) { return d.r; })
+                      .style("fill", function(d) { return color(d.packageName); });
+                 
+                    /*
+                  var innerSVG = node.append("g")
+                        .attr("transform", function(d) { return "translate(" + 0.25*1,8*d.r + ")"; })
+                        .attr("width", function(d) { return 1.8*d.r; })
+                        .attr("heigth", function(d) { return 1.8*d.r; });
+                  */
+                  node.append("text")
+                        .attr("id", function (d) { return "modTitle_" + d.originData.cmid;})
+                        .style("text-anchor", "middle")
+                        .text(function(d) { console.log(d); return d.className.substring(0, d.r / 3); });
+                   
+                  node.append("image")
+                        .attr("xlink:href", function (d) { return d.originData.iconURL;})
+                        .attr("x", function(d) {
+                            var text = d3.select("#modTitle_" + d.originData.cmid);
+                            console.log(text[0][0]);
+                            var width = text[0][0].clientWidth;
+                            return -0.5*width - 28;
+                            }
+                        )
+                        .attr("y", -24)
+                        .attr("height", 24)
+                        .attr("width", 24);
+               }
+                process(null, json);
+                // Returns a flattened hierarchy containing all leaf nodes under the root.
+                function classes(root) {
+                  var classes = [];
+
+                  function recurse(name, node) {
+                    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+                    else classes.push({packageName: name, className: node.name, value: node.size, originData: node});
+                  }
+ 
+                  recurse(null, root);
+                  return {children: classes};
+                }
+
+                d3.select(self.frameElement).style("height", diameter + "px");
+
+                </script>';
+        //$this->printHTML($buffer);
         echo $buffer;
-        //echo "</body></html>";
-        //echo '<svg height="300" width="100%"><text fill="#000000" font-size="45" font-family="Verdana" x="50" y="86">SVG</text>';
+    }
+
+    public function printHTML($var) {
+        $dom = new DOMDocument();
+        $dom->preserveWhiteSpace = FALSE;
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($var);
+        libxml_clear_errors();
+        $dom->formatOutput = TRUE;
+
+        echo $dom->saveXML();
     }
 
     public function printArray($var) {
